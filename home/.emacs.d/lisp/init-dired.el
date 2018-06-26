7c77a9c46bcc84e57a916512d8fd5f9bfc41e452
Ken Wong <canonw@users.noreply.github.com>
2018-06-26 02:48:50 +0000

    Minor update to emacs dired.

--- home/.emacs.d/lisp/init-dired.el
+++ home/.emacs.d/lisp/init-dired.el
@@ -25,23 +25,23 @@
   (setq peep-dired-ignored-extensions '("mkv" "iso" "mp4" "mp3"))
   )
 
-(use-package dired+
-  :after dired
-  :defer t
-  ;; :config
-  ;; (setq diredp-hide-details-initially-flag nil)
-  ;; (set-face-foreground 'diredp-file-name nil)
-  ;; ;; Keep dired buffers updated when the file system changes.
-  ;; (setq global-auto-revert-non-file-buffers t)
-  ;; (setq auto-revert-verbose nil)
-  )
-
-(use-package dired-details
-  :after dired
-  :defer t)
-  
-(use-package dired-sort
-  :after dired
-  :defer t)
+;; (use-package dired+
+;;   :after dired
+;;   :defer t
+;;   ;; :config
+;;   ;; (setq diredp-hide-details-initially-flag nil)
+;;   ;; (set-face-foreground 'diredp-file-name nil)
+;;   ;; ;; Keep dired buffers updated when the file system changes.
+;;   ;; (setq global-auto-revert-non-file-buffers t)
+;;   ;; (setq auto-revert-verbose nil)
+;;   )
+;; 
+;; (use-package dired-details
+;;   :after dired
+;;   :defer t)
+;;   
+;; (use-package dired-sort
+;;   :after dired
+;;   :defer t)
 
 (provide 'init-dired)
